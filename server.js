8f313cff3690f099f889539269e011598225e0c1
gabit <gabit690@gmail.com>
2021-12-15 18:05:25 -0300

    Add first GET route

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 const express = require('express');
 const app = express();
 
+const mockUserData = [
+    {name:"Mark"},
+    {name:"Jill"}
+];
+
+app.get("/users", (req, res) => {
+    res.json({
+        success: true,
+        message: "Successfully got users. Nice!",
+        users: mockUserData
+    });
+});
+
 const PORT = 8000;
 
 app.listen(PORT, () => {
